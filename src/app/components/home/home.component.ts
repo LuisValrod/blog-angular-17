3f4e3c9f07fafd41fd65693de044e6c8b0ada990
LuisValrod <luisrodriguezvalido@gmail.com>
2024-03-08 20:13:46 +0000

    routing with routeLink

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { PostPreviewComponent } from './components/post-preview/post-preview.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, PostPreviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/types/post.types.ts
@@ -0,0 +1,10 @@
+export type Post = {
+    title: string,
+    subtitle: string,
+    thumbnail: string,
+    publicationDate: string,
+    slug: string,
+    author: string,
+    content: string
+
+}
