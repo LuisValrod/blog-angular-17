66d6cb409eb74b3f9e1239cfb79f9efbda19ba6a
LuisValrod <luisrodriguezvalido@gmail.com>
2024-03-08 20:57:32 +0000

    headerService and types implemented

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderData, HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  private uiDAta: HeaderData = {
+    title: 'About me',
+    subtitle: 'Another subtitle',
+    thumbnail: 'https://placeholder.co/600x400'
+  }
 
+  constructor (private headerService: HeaderService) {
+    headerService.uiData.set(this.uiDAta)
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { PostPreviewComponent } from './components/post-preview/post-preview.component';
+import { HeaderData, HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,20 @@ import { PostPreviewComponent } from './components/post-preview/post-preview.com
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private uiData: HeaderData = {
+    title: 'My personal blog',
+    subtitle: 'This is the subtitle',
+    thumbnail: 'https://placehold.co/600x400'
 
+  }
+  constructor (private headerService: HeaderService){}
+
+    ngOnInit(){
+
+      this.headerService.uiData.set(this.uiData)
+
+    }
+
+  
 }
